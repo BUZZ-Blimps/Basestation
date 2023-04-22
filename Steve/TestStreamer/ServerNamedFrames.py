ed97b7ed3386f7436c9c4821840b10e4234822ff
swampblimps <swampblimps@gmail.com>
2023-04-21 20:31:35 -0400

    added blimp names, tweaked streaming bugs (dropping frames)

--- Steve/TestStreamer/ServerNamedFrames.py
+++ Steve/TestStreamer/ServerNamedFrames.py
@@ -7,16 +7,35 @@ import socket
 import struct
 import time
 
-MAX_DGRAM = 2**16
+MAX_DGRAM = 2**14
+
+blimpIPNameMap = { "192.168.0.101": "Spicy Hot Dog",
+                   "192.168.0.102": "Waffle",
+                   "192.168.0.103": "Apple",
+                   "192.168.0.104": "Milk",
+                   "192.168.0.105": "Pasta",
+
+                   "192,168.0.100": "Silly Ahh",
+
+                   "192.168.0.80": "Big Cup of Eggs",
+                   "192.168.0.20": "Leg in a Cup",
+                   "192.168.0.89": "I'm in a Cup",
+                   "192.168.0.62": "My Cup of Eggs",
+                   "192.168.0.86": "Pint of Eggs",
+                   "192.168.0.14": "Stealthy Steve",
+
+                   "192.168.0.38": "Barometer"}
 
 def dump_buffer(s):
     """ Emptying buffer frame """
+    s.setblocking(False)
     while True:
-        seg, addr = s.recvfrom(MAX_DGRAM)
-        print(seg[0])
-        if struct.unpack("B", seg[0:1])[0] == 1:
-            print("finish emptying buffer")
+        try:
+            seg, addr = s.recvfrom(MAX_DGRAM)
+        except BlockingIOError:
+            print("Finished emptying buffer")
             break
+    s.setblocking(True)
 
 def main():
     """ Getting image udp frame &
@@ -40,27 +59,48 @@ def main():
 
     while True:
         seg, addr = s.recvfrom(MAX_DGRAM)
+        address = str(addr[0])
+        if address in blimpIPNameMap:
+            blimpName = blimpIPNameMap[address]
+        else:
+            blimpName = address
+
         # print("Received data")
+        frameNum = struct.unpack("B", seg[0:1])[0]
+        #print(frameNum, ", ",sep='',end='')
         if struct.unpack("B", seg[0:1])[0] > 1:
             dat += seg[1:]
         else:
+            #print()
             dat += seg[1:]
             # Get size of frameName (first byte)
             frameNameSize = int(dat[0])
-            frameName = bytes.decode(dat[1:(1+frameNameSize)], "utf-8")
+            try:
+                frameName = bytes.decode(dat[1:(1+frameNameSize)], "utf-8")
+            except:
+                print("bytes.decode failed (dat[0:10] = ",dat[0:10],sep='')
+                dump_buffer(s)
+                dat = b''
+                continue
+
             #print("frameNameSize =",frameNameSize,"\t\tframeName =",frameName)
             dat = dat[(1+frameNameSize):]
+
+            try:
+                img = cv2.imdecode(np.fromstring(dat, dtype=np.uint8), 1)
+
+                if (img is not None):
+                    cv2.imshow(blimpName + ": "+frameName, img)
+            except:
+                print("imdecode failed")
+                dat = b''
+                continue
 
             # Increment frame count
             if frameName in frameCounts:
                 frameCounts[frameName] += 1
             else:
                 frameCounts[frameName] = 1
-
-            img = cv2.imdecode(np.fromstring(dat, dtype=np.uint8), 1)
-
-            if (img is not None):
-                cv2.imshow("Frame: "+frameName, img)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
