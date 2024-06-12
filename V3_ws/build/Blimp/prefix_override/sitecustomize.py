71fbd732c322b7301bf24bbbf29c50c39fffdc8c
Sahaj Patel <sah4j.patel@gmail.com>
2024-06-11 21:23:33 -0500

    update V2 branch

--- V3_ws/build/Blimp/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/local/GitHub/Basestation/V3_ws/install/Blimp'
--- V3_ws/build/my_package/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/coreblimp1/GitHub/Basestation/V3_ws/install/my_package'
