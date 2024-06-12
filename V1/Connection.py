41fb65d58c00ff87fb923263fbb85fa0636cefb5
Sahaj Patel <sah4j.patel@gmail.com>
2024-06-11 21:26:27 -0500

    update V3 branch

--- V1/Connection.py
+++
@@ -1,7 +0,0 @@
-class Connection:
-    def __init__(self, inputName, blimpName, inputIndex, blimpIndex):
-        self.inputName = inputName
-        self.blimpName = blimpName
-        self.inputIndex = inputIndex
-        self.blimpIndex = blimpIndex
-        self.names = (inputName,blimpName)
